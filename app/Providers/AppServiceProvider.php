fd779ee1eac38dd3578bfac7bae539febac4689c
David Trushkov <davidtrushkov@users.noreply.github.com>
2020-10-27 19:26:53 -0400

    Dropbox clone with Laravel Jetstream and Livewire

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\File;
+use App\Models\Folder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Relation::morphMap([
+            'folder' => Folder::class,
+            'file' => File::class,
+        ]);
+    }
+}
